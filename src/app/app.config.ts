01b596f2ad4ff5cea126a5e210a6fec9bc2343c3
ishwar-cyber <61781204+ishwar-cyber@users.noreply.github.com>
2025-08-20 14:50:45 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,27 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideAppInitializer } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { provideToastr } from 'ngx-toastr';
 import { CartService } from './services/cart';
 import { authInterceptor } from './commons/interceptors/auth-interceptor';
 import { isPlatformBrowser } from '@angular/common';
+// ✅ Startup function
+function loadCartOnInit() {
+  const http = inject(HttpClient);
+  const cartService = inject(CartService);
+
+  return () =>
+    firstValueFrom(
+      http.get('https://your-api.com/api/v1/cart', { withCredentials: true }).pipe(
+        tap((cart: any) => {
+          cartService.setCart(cart); // store in signals/store
+        })
+      )
+    );
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
@@ -19,7 +35,10 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), provideClientHydration(withEventReplay()),
     provideAnimations(), // required animations providers
     provideToastr(), // Toastr providers
-    CookieService
+    CookieService,
+    // ✅ New Angular 20 way
+    provideAppInitializer(loadCartOnInit),
+  ],
   ]
 };
 
