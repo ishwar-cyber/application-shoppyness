81cb278749384da8f692896f823c7f51c23d5094
ishwar-cyber <panditishwar21@gmail.com>
2025-12-27 11:26:21 +0530

    remove cart from start load

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
 import { NetworkService } from './services/network-service';
+import { CartService } from './services/cart';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,14 @@ import { NetworkService } from './services/network-service';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit{
+
   protected title = 'application-shoppyness';
 
   public network = inject(NetworkService);
+  private cartService = inject(CartService);
+
+   ngOnInit(): void {
+    this.cartService.loadCart();
+  }
 }
