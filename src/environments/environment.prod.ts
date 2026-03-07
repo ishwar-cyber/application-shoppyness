692c4a637cb1513a202e063543b0b02c47ce888f
ishwar-cyber <panditishwar21@gmail.com>
2026-03-07 07:20:38 +0530

    update environment URLs for production and development configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://shoppyness-backend.onrender.com/api/v1',
+  // apiUrl: 'https://shoppyness-backend.onrender.com/api/v1',
+  apiUrl: 'https://dashboard-production-5fa5.up.railway.app/api/v1',
 };
